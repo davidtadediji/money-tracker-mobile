2a606605a8bf1ce901f438e062800439f7e28071
David Adediji <david.adediji@sil.mu>
2025-10-23 22:02:25 +0400

    Testing

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,8 @@ import Landing from '@/app/landing';
 const isLoggedIn = true; // mock flag
 
 export default function Index() {
-  if (isLoggedIn) {
-    return <Redirect href="/(tabs)" />;
-  }
+  // if (isLoggedIn) {
+  //   return <Redirect href="/(tabs)" />;
+  // }
   return <Landing />;
 }
